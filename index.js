c4d7051f9a94d4244f7942e190d4463250eeb9fc
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-05-07 15:45:53 +0530

    Added auto reply with jokes functionality

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let http = require('http');
 const port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
-    // retweet()
-    tweetEvent()
+    retweet()
+    reply()
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello I am Bot Pandoh, you should not be here. Are you lost? <a href="https://twitter.com/BotPandoh">Click here</a> to see me working live ');
 }).listen(port);
@@ -15,40 +15,79 @@ let config = {
     consumer_secret: process.env.CONSUMER_SECRET,
     access_token: process.env.ACCESS_TOKEN,
     access_token_secret: process.env.ACCESS_TOKEN_SECRET
+
 }
+// let config = require('./config')
 
 let Twitter = new twit(config);
-let stream = Twitter.stream('user');
-stream.on('tweet', tweetEvent);
+let lastId
 
-function tweetEvent(tweet){
-    let reply_to = tweet.in_reply_to_screen_name;
-    let name = tweet.user.screen_name;
-    let txt = tweet.text;
-    if (reply_to === 'BotPandoh') {
+//Getting joke
+var request = require('request');
 
-        // Get rid of the @ mention
-        txt = txt.replace(/@BotPandoh/g,'');
-    
-        // Start a reply back to the sender
-        var reply = '.@'+name + ' ';
-        // Reverse their text
-        for (var i = txt.length-1; i >= 0; i--) {
-          reply += txt.charAt(i);
+let options = {
+    url: 'https://official-joke-api.appspot.com/jokes/programming/random',
+    method: 'GET'
+}
+let joke
+request(options, (err, response, body) => {
+    if(!err && response.statusCode == 200)
+     joke = JSON.parse(body)
+     joke =  joke[0].setup + " " +joke[0].punchline
+    console.log(joke)
+});
+
+function reply(){
+    let params = {
+        q: '@BotPandoh',
+        result_type: 'recent',
+        lang: 'en'    
+        } 
+    Twitter.get('search/tweets', params, function(err, data) {
+
+        if (!err) {
+          // Get's id of tweet to reply
+            var tweetId = data.statuses[0].id_str;
+            //Checks if already replied or not
+            if(tweetId == lastId ){
+                console.log("already replied")
+            }
+            else{
+                // Replying happens here
+                let x = data.statuses[0].user.screen_name
+                Twitter.post('statuses/update', {
+                in_reply_to_status_id:tweetId,
+                status:"@" + x + " @" + x + " " + joke
+            }, function(err, response) {
+                if (response) {
+                    console.log('Replied' + tweetId);
+                    lastId = tweetId
+                }
+                //If Error while tweeting
+                if (err) {
+                    console.log('Something went wrong while Replying...'+ params.q);
+                }
+            });
+            // Liking happens here
+            Twitter.post('favorites/create', {
+                id: tweetId
+            }, function(err, response) {
+                if (response) {
+                    console.log('Liked tweet from' + params.q);
+                }
+                //If Error while liking
+                if (err) {
+                    console.log('Something went wrong while Liking...'+ params.q +' Already Liked maybe...');
+                }
+            });
+            }
+
         }
-      
-        // Post that tweet!
-        T.post('statuses/update', { status: reply }, tweeted);
-    
-        // Make sure it worked!
-        function tweeted(err, reply) {
-          if (err !== undefined) {
-            console.log(err);
-          } else {
-            console.log('Tweeted: ' + reply);
-          }
-        };
-      }
+        // if unable to Search a tweet
+        else {
+          console.log('Something went wrong while SEARCHING...' + params.q);
+        }
+    });
 }
 
 let retweet = function() {
