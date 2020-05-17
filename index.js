6170ed488029fcf1335e7f6f4802694622920709
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-05-17 20:40:09 +0530

    code refactored and improved

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 let http = require('http');
+const fs = require('fs');
+
 const port = process.env.PORT || 3000;
+
 http.createServer(function (req, res) {
-    retweet()
-    reply()
+    reply();
+    retweet();
+    like();
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello I am Bot Pandoh, you should not be here. Are you lost? <a href="https://twitter.com/BotPandoh">Click here</a> to see me working live ');
 }).listen(port);
 
 let twit = require('twit');
 
-// Getting keys from env values
+/* Getting keys from env values */
+
 let config = {
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
@@ -17,14 +22,18 @@ let config = {
     access_token_secret: process.env.ACCESS_TOKEN_SECRET
 
 }
-// let config = require('./config')
+
+/* Getting keys and tokens if they are stored in local config file   */
+/* let config = require('./config') */
 
 let Twitter = new twit(config);
-let lastId
-let joke
+let joke;
+let tagsArray = ['#fullstack', '#100DaysOfCode', '#javascript', '#programming', '#coding', '#webdevelopment', '#NodeJS'];
 
+/* This function replies to mentioned tweet */
 function reply(){
-    //Getting joke
+
+    /* Getting joke */
     let request = require('request');
 
     let options = {
@@ -36,105 +45,84 @@ function reply(){
             joke = JSON.parse(body)
             joke =  joke[0].setup + " " +joke[0].punchline
             console.log(joke)
-        });
+    });
     let params = {
-        q: '@BotPandoh',
-        result_type: 'recent',
-        lang: 'en'    
-        } 
+            q: '@BotPandoh',
+            result_type: 'recent',
+            lang: 'en'    
+    }
+
     Twitter.get('search/tweets', params, function(err, data) {
 
         if (!err) {
-          // Get's id of tweet to reply
-            var tweetId = data.statuses[0].id_str;
-            //Checks if already replied or not
-            if(tweetId == lastId ){
-                console.log("already replied")
-            }
-            else{
-                // Replying happens here
-                let x = data.statuses[0].user.screen_name
-                Twitter.post('statuses/update', {
-                in_reply_to_status_id:tweetId,
-                status:"@" + x + " @" + x + " " + joke
-            }, function(err, response) {
-                if (response) {
-                    console.log('Replied' + tweetId);
-                    lastId = tweetId
-                }
-                //If Error while tweeting
-                if (err) {
-                    console.log('Something went wrong while Replying...'+ params.q);
-                }
-            });
-            // Liking happens here
-            Twitter.post('favorites/create', {
-                id: tweetId
-            }, function(err, response) {
-                if (response) {
-                    console.log('Liked tweet from' + params.q);
-                }
-                //If Error while liking
-                if (err) {
-                    console.log('Something went wrong while Liking...'+ params.q +' Already Liked maybe...');
-                }
-            });
-            }
+          /* Get id of tweet to reply */
+            let tweetId = data.statuses[0].id_str;
+            let check = 0;
 
+            /* Checking if already replied to tweet or not */
+            fs.readFile('./repliedId.json', function (err, OldData) {
+                let dataArray = JSON.parse(OldData);
+                for(let i = 0; i < dataArray.length; i++){
+                    if(tweetId === dataArray[i].tweetId){
+                        check = 1;
+                        break;
+                    }
+                }
+                if(check === 1){
+                    console.log("already replied");
+                }
+                else{
+                    /* Replying happens here */
+                    let x = data.statuses[0].user.screen_name;
+                    Twitter.post('statuses/update', { in_reply_to_status_id:tweetId, status:"@" + x + " @" + x + " " + joke}, function(err, response) {
+                        if (response) {
+                            console.log('Replied' + tweetId);
+                            let obj = {
+                                tweetId : tweetId
+                            }
+                            dataArray.push(obj);
+                            console.log("here " + JSON.stringify(dataArray));
+                            fs.writeFile("./repliedId.json", JSON.stringify(dataArray), function(err){
+                              if (err) throw err;
+                              console.log('The id was sucessfully appened to file ' + tweetId);
+                            });
+                        }
+                        /* If Error while tweeting */
+                        if (err) {
+                            console.log('Something went wrong while Replying...'+ params.q);
+                        }
+                    });
+
+                    /* Liking happens here */
+                    Twitter.post('favorites/create', { id: tweetId }, function(err, response) {
+                        if (response) {
+                        console.log('Liked tweet from' + params.q);
+                        }
+                        /* If Error while liking */
+                        if (err) {
+                        console.log('Something went wrong while Liking...'+ params.q +' Already Liked maybe...');
+                        }
+                    });
+                }
+            });
         }
-        // if unable to Search a tweet
+        /* If some error happens */
         else {
-          console.log('Something went wrong while SEARCHING...' + params.q);
+          console.log('Some error happened');
         }
     });
 }
 
+/* This function retweets a random tweet from tagsArray  */
 let retweet = function() {
     let i 
     let parms
-    i = Math.floor(Math.random() * 5) + 1
-    if(i == 0){
-        params = {
-            q: '#fullstack',
-            result_type: 'recent',
-            lang: 'en'    
-        } 
-    }
-    else if(i == 1){
-        params = {
-        q: '#100DaysOfCode',
+    i = Math.floor(Math.random() * 6) + 1
+    params = {
+        q: tagsArray[i],
         result_type: 'recent',
         lang: 'en'    
-        } 
-    }
-    else if(i == 2){
-        params = {
-            q: '#javascript',
-            result_type: 'recent',
-            lang: 'en'    
-        } 
-    }
-    else if(i == 3){
-        params = {
-            q: '#programming',
-            result_type: 'recent',
-            lang: 'en'    
-        } 
-    }
-    else if(i == 4){
-        params = {
-            q: '#coding',
-            result_type: 'recent',
-            lang: 'en'    
-        } 
-    }
-    else {
-        params = {
-            q: '#webdevelopment',
-            result_type: 'recent',
-            lang: 'en'    
-        } 
-    }
+    } 
 
     Twitter.get('search/tweets', params, function(err, data) {
 
@@ -142,91 +130,50 @@ let retweet = function() {
             // Get's id of tweet to retweet
               var retweetId = data.statuses[0].id_str;
               // Retweeting happens here
-              Twitter.post('statuses/retweet/:id', {
-                  id: retweetId
-              }, function(err, response) {
+              Twitter.post('statuses/retweet/:id', { id: retweetId }, function(err, response) {
                   if (response) {
                       console.log('Retweeted!!!' + params.q);
-                      like()
                   }
-                  //If Error while tweeting
+                  /* If Error while tweeting */
                   if (err) {
                       console.log('Something went wrong while RETWEETING...'+ params.q +' Duplication maybe...');
                   }
               });
           }
-          // if unable to Search a tweet
+          /* If unable to Search tweet */
           else {
             console.log('Something went wrong while SEARCHING...' + params.q);
           }
       });
   }
 
+/* This function likes a random tweet from tagsArray  */
 let like = function(){
-    let i 
-    let parms
-    i = Math.floor(Math.random() * 5) + 1
-    if(i == 0){
-        params = {
-            q: '#fullstack',
-            result_type: 'recent',
-            lang: 'en'    
-        } 
-    }
-    else if(i == 1){
-        params = {
-        q: '#100DaysOfCode',
+    let i; 
+    let parms;
+    i = Math.floor(Math.random() * 6) + 1;
+    params = {
+        q: tagsArray[i],
         result_type: 'recent',
         lang: 'en'    
-        } 
-    }
-    else if(i == 2){
-        params = {
-            q: '#javascript',
-            result_type: 'recent',
-            lang: 'en'    
-        } 
-    }
-    else if(i == 3){
-        params = {
-            q: '#programming',
-            result_type: 'recent',
-            lang: 'en'    
-        } 
-    }
-    else if(i == 4){
-        params = {
-            q: '#coding',
-            result_type: 'recent',
-            lang: 'en'    
-        } 
-    }
-    else {
-        params = {
-            q: '#webdevelopment',
-            result_type: 'recent',
-            lang: 'en'    
-        } 
-    }
+    } 
     Twitter.get('search/tweets', params, function(err, data) {
 
         if (!err) {
           // Get's id of tweet to like
             let favId = data.statuses[0].id_str;
             // Liking happens here
-            Twitter.post('favorites/create', {
-                id: favId
-            }, function(err, response) {
+            Twitter.post('favorites/create', { id: favId }, function(err, response) {
                 if (response) {
                     console.log('Liked tweet from tag' + params.q);
                 }
-                //If Error while liking
+                /* If Error while liking */
                 if (err) {
                     console.log('Something went wrong while Liking...'+ params.q +' Already Liked maybe...');
                 }
             });
         }
-        // if unable to Search a tweet
+        /* If unable to Search tweet */
         else {
           console.log('Something went wrong while SEARCHING...' + params.q);
         }
