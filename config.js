b548c00035a667a6646efe5a6a6a38f91667b259
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-05-20 00:17:26 +0530

    config file removed

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: '61zhflY4hvE4GUwaKEsP8uKvP',  
-    consumer_secret: 'mqcfx6RsZD1t1CMivvwIUskDgfaUP0GaEnhNVpk0qIy82UE1jv',
-    access_token: '1256946354905063424-U3Ya2ZtZ7061HYfE7bM06py99o6iwb',  
-    access_token_secret: 'gsVJMo5nqs28QkxmWqvhLFajvxAbQ955XsZl7koOG23aX'
-  }
