9373bafd5921a32f060ceca44a47cd4f074821c4
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-05-29 16:56:25 +0530

    added fetching tracks from API and tweeting functionaltity

--- index.js
+++ index.js
@@ -1,97 +1,89 @@
 let http = require('http');
 const fs = require('fs');
 
-const port = process.env.PORT || 3000;
+let request = require('request');
+
+const port = process.env.PORT || 5000;
 
 http.createServer(function (req, res) {
-    // reply();
-    retweet();
-    like();
+    initiateTweet()
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello I am Bot Pandoh, you should not be here. Are you lost? <a href="https://twitter.com/BotPandoh">Click here</a> to see me working live ');
 }).listen(port);
 
 let twit = require('twit');
 
-/* Getting keys from env values for @MusicSmokinBot */
-
 let config = {
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 
 }
 
-/* Getting keys and tokens if they are stored in local config file   */
-/* let config = require('./config') */
+let apiKey = '104aa872cddd1e64693510ca20f37b34';
+let genre = 18;
+let snippet = true;
+let language = 'en';
+let baseURL = 'https://api.musixmatch.com/ws/1.1';
 
-let Twitter = new twit(config);
-let tagsArray = ['#hiphop', '#rap', '#music', '#hiphopmusic', '#hiphopculture', '#rapartists', '#hop', '#drake', '#future' , '#ynwmelly' , '#weeknd', '#liluzivert' , '#xxlfreshmen' ,'#JuiceWrld' , '#Jaden' , '#LilTjay' , '#TuPac' , '#PoloG' , "#MetroBoomin" , '#21Savage' , '#soundcloud' , '#hiphopnation'];
-
-/* This function retweets a random tweet from tagsArray  */
-let retweet = function() {
-    let i 
-    let parms
-    i = Math.floor(Math.random() * 21) + 1
-    params = {
-        q: tagsArray[i],
-        result_type: 'recent',
-        lang: 'en'    
-    } 
-
-    Twitter.get('search/tweets', params, function(err, data) {
-
-          if (!err) {
-            // Get's id of tweet to retweet
-              var retweetId = data.statuses[0].id_str;
-              // Retweeting happens here
-              Twitter.post('statuses/retweet/:id', { id: retweetId }, function(err, response) {
-                  if (response) {
-                      console.log('Retweeted!!!' + params.q);
-                  }
-                  /* If Error while tweeting */
-                  if (err) {
-                      console.log('Something went wrong while RETWEETING...'+ params.q +' Duplication maybe...');
-                  }
-              });
-          }
-          /* If unable to Search tweet */
-          else {
-            console.log('Something went wrong while SEARCHING...' + params.q);
-          }
-      });
-  }
-
-/* This function likes a random tweet from tagsArray  */
-let like = function(){
-    let i; 
-    let parms;
-    i = Math.floor(Math.random() * 21) + 1;
-    params = {
-        q: tagsArray[i],
-        result_type: 'recent',
-        lang: 'en'    
-    } 
-    Twitter.get('search/tweets', params, function(err, data) {
-
-        if (!err) {
-          // Get's id of tweet to like
-            let favId = data.statuses[0].id_str;
-            // Liking happens here
-            Twitter.post('favorites/create', { id: favId }, function(err, response) {
-                if (response) {
-                    console.log('Liked tweet from tag' + params.q);
-                }
-                /* If Error while liking */
-                if (err) {
-                    console.log('Something went wrong while Liking...'+ params.q +' Already Liked maybe...');
-                }
-            });
+function initiateTweet(){
+  let url = `${baseURL}/track.search?format=json&apikey=${apiKey}&s_track_rating=desc`;
+  if (genre) { url += `&f_music_genre_id=${genre}`; }
+  if (snippet) { url += `&f_has_lyrics=1`; }
+  if (language) { url += `&f_lyrics_language=${language}`; }
+  
+  // console.log(url)
+  request.get(url + `&page_size=1`, function(error, response, body) {
+    body = JSON.parse(body);
+    let available = body.message.header.available;
+    // console.log(available);
+    if(available > 0){
+      var pages = Math.ceil(available / 100);
+      var page = Math.floor((Math.random() * pages) + 1);
+      request.get(url + `&page_size=100&page=${page}`, function(error, response, body) {
+        body = JSON.parse(body);
+        var tracks = body.message.body.track_list;
+        // console.log(tracks.length)
+        if(tracks.length > 0){
+          var rnd = Math.floor((Math.random() * tracks.length));
+          let trackDetail = tracks[rnd]
+          getAlldetails(trackDetail)
         }
-        /* If unable to Search tweet */
-        else {
-          console.log('Something went wrong while SEARCHING...' + params.q);
-        }
-    });
+      })
+    }
+  
+  })
+}
+
+function getAlldetails(x){
+  var url = `${baseURL}/track.snippet.get?format=json&apikey=${apiKey}`;
+  // console.log(x.track.album_id)
+  url += `&artist_id=${x.track.artist_id}`
+  url += `&album_id=${x.track.album_id}`
+  url += `&track_id=${x.track.track_id}`
+  request.get(url, function(error, response, body){
+     body = JSON.parse(body);
+
+    let details = {
+      lyricSnippet : body.message.body.snippet.snippet_body,
+      track : x.track.track_name,
+      artist : x.track.artist_name,
+      album : x.track.album_name
+    }
+    console.log(details)
+    goTweet(details)
+  })
+}
+
+function goTweet(details){
+  Twitter.post('statuses/update', { status:"This is a test tweet"}, function(err, response) {
+    if (response) {
+        console.log("Tweeted!" + " " + details);
+    }
+    /* If Error while tweeting */
+    if (err) {
+        console.log('Something went wrong while Replying...'+ params.q);
+    }
+});
 }
